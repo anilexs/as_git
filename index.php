1b9bad88b6c66b762160ae215c13327d6e0119b3
alexis <alexissimoes94@gmail.com>
2023-07-27 15:06:28 +0200

    v1

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.0/dist/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Document</title>
+</head>
+<body>
+    <div class="helpDiv">
+        <img src="img/silica_help.png" alt="" id="help">
+        <!-- <div class="bubble">hello dedfefefefefefefe</div> -->
+        
+    </div>
+    <div class="sakura"></div>
+  <script src="js/structure.js"></script>
+    <div class="form">
+        <form action="form">
+            <label for="nam">First nam</label>
+            <input id="nam" type="text" placeholder="First nam"> <br>
+
+            <label for="nam2">last nam</label>
+            <input id="nam2" type="text" placeholder="last nam"> <br>
+
+            <label for="mail">E-mail</label>
+            <input id="mail" type="text" placeholder="First nam"> <br>
+
+            <input type="submit" id="submit">
+           <button id="google"><img class="img1" src="img/google.png" alt="">   sincrire avec google</button>
+        </form>
+    </div>
+</body>
+</html>
